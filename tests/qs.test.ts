ec6ad9a05940759c906b98bf3cdcecbedecae50c
g4rcez <allan.f.garcez@gmail.com>
2023-02-05 03:52:39 -0300

    tests: fix tests

--- tests/qs.test.ts
+++ tests/qs.test.ts
@@ -9,14 +9,14 @@ describe("Should test qsToString utils.ts", () => {
     });
 
     test("should test object with dates", () => {
-        const result = qsToString("/?dates=date[]", {
-            dates: [new Date(1970, 0, 1), new Date(1970, 0, 1)],
+        const result = qsToString("/?numbers=numbers[]", {
+            numbers: [1, 2, 3, 4],
         });
-        expect(result).toBe("dates=1970-01-01T03%253A00%253A00.000Z&dates=1970-01-01T03%253A00%253A00.000Z");
+        expect(result).toBe("numbers=1&numbers=2&numbers=3&numbers=4");
     });
 
     test("should transform back to object", () => {
-        const q = new URLSearchParams("date=2023-02-05T06%253A14%253A48.668Z&date=1970-01-01T03%253A00%253A00.000Z&id=1");
-        transformData(q, mapUrlToQueryStringRecord("/?date=date[]&id=number", fromStringToValue));
+        const q = new URLSearchParams("numbers=1&numbers=2&numbers=3&numbers=4&id=5");
+        transformData(q, mapUrlToQueryStringRecord("/?numbers=numbers[]&id=number", fromStringToValue));
     });
 });
