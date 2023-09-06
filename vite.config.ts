b42ee5f166c570d93611bd7528966bf109fc55ae
g4rcez <allan.f.garcez@gmail.com>
2023-09-06 01:32:06 -0300

    chore: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         },
         rollupOptions: {
             treeshake: true,
-            external: ["react/jsx-runtime", "react"],
+            external: ["react"],
         },
     },
 });
